7937dc05cf0a2ecd5f45f3451d756c85c260e0f8
Domenic Denicola <d@domenic.me>
2015-03-16 21:44:18 +0900

    Initial job, with hacks, that seems to work! Some algorithms converted!

--- ecmarkdownify.js
+++ ecmarkdownify.js
@@ -3,9 +3,18 @@ const jsdom = require('jsdom');
 const Spec = require('ecmarkup/lib/Spec');
 const Algorithm = require('ecmarkup/lib/Algorithm');
 
-const inputPath = process.argv[2];
+const inputPath = 'index.html';
+const outputPath = 'index2.html';
 
 const doc = jsdom.jsdom(fs.readFileSync(inputPath, { encoding: 'utf-8' }));
+
+const algs = Array.from(doc.querySelectorAll('pre[is="emu-alg"]'));
+for (const preAlg of algs) {
+  const realAlg = doc.createElement('emu-alg');
+  realAlg.innerHTML = preAlg.innerHTML;
+  preAlg.parentNode.replaceChild(realAlg, preAlg);
+}
+
 const spec = new Spec(inputPath, fetch, doc);
 
 Promise.all([
@@ -13,6 +22,10 @@ Promise.all([
   spec.loadBiblios()
 ])
 .then(() => spec.buildAlgs())
+.then(() => {
+  const result = jsdom.serializeDocument(doc);
+  fs.writeFileSync(outputPath, result);
+})
 .catch(e => process.nextTick(() => { throw e; }));
 
 
