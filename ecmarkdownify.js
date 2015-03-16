fb81dea9bf8c078ccb194909a16d6f73b3c7a020
Domenic Denicola <d@domenic.me>
2015-03-16 22:12:07 +0900

    Add comments to build script to prevent people from thinking I'm totally crazy

--- ecmarkdownify.js
+++ ecmarkdownify.js
@@ -3,11 +3,15 @@ const jsdom = require('jsdom');
 const Spec = require('ecmarkup/lib/Spec');
 const Algorithm = require('ecmarkup/lib/Algorithm');
 
+// Hard code for now because traceur-runner is not happy with command line arguments
 const inputPath = 'index.html';
 const outputPath = 'index2.html';
 
 const doc = jsdom.jsdom(fs.readFileSync(inputPath, { encoding: 'utf-8' }));
 
+// Work around https://github.com/tabatkins/bikeshed/issues/380 by authoring the .bs file with <pre is="emu-alg">
+// (which means the input to this script contains <pre is="emu-alg">) then converting to real <emu-alg> before
+// passing to Ecmarkup.
 const algs = Array.from(doc.querySelectorAll('pre[is="emu-alg"]'));
 for (const preAlg of algs) {
   const realAlg = doc.createElement('emu-alg');
@@ -22,6 +26,7 @@ Promise.all([
   spec.loadBiblios()
 ])
 .then(() => {
+  // Allow aoid="" anywhere. Ecmarkup only likes it on <emu-clause> and <emu-alg>, I think.
   const aos = Array.from(doc.querySelectorAll('[aoid]'));
   for (const ao of aos) {
     const aoid = ao.getAttribute('aoid');
